f997e8669c3177a284506f5bec57288d681404f6
badalOraon-06 <btech10738.22@bitmesra.ac.in>
2026-05-29 19:37:38 +0530

    Add implementation of heap data structure with insertion and print functionality

---
+++ 09_Heap/01_Learning/Insertion.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+
+using namespace std;
+
+class heap{
+  public:
+  int arr[100];
+  int size = 0;
+
+  heap()
+  {
+    arr[0] = -1;
+    size = 0;
+  }
+
+  void insert(int val)
+  {
+    size++;
+    int index = size;
+    arr[index] = val;
+
+    while(index > 1)
+    {
+      int parent = index / 2;
+
+      if(arr[parent] < arr[index])
+      {
+        swap(arr[parent], arr[index]);
+        index = parent;
+      }
+      else{
+        return;
+      }
+    }
+  }
+
+  void print(){
+    for(int i = 1; i <= size; i++)
+    {
+      cout<< arr[i] << " ";
+    }
+  }
+};
+
+int main()
+{
+  heap h;
+
+  h.insert(60);
+  h.insert(50);
+  h.insert(40);
+  h.insert(30);
+  h.insert(20);
+  h.insert(55);
+  h.insert(70);
+
+  h.print();
+
+  return 0;
+}
